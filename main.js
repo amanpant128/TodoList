1110e49e19c3d46df55ed7a5cb2eb825a5cd7d16
Aman Pant <amanpant@Amans-MacBook-Air.local>
2020-02-02 23:31:28 +0530

    todo_list

---
+++ main.js
@@ -0,0 +1,24 @@
+function taskadd() {
+    taskNew = new objectTask();
+}
+
+function objectTask() {
+    var task = document.getElementById('textinput').value;
+    var listItem = document.createElement("li");
+    var checkbox = document.createElement("input")
+    checkbox.type = "checkbox";
+    listItem.appendChild(checkbox);
+    var itemText = document.createTextNode(task);
+    listItem.appendChild(itemText);
+    document.getElementById('place').appendChild(listItem);
+}
+
+function deleteCheckBox() {
+    var ul = document.getElementById('place');
+    var li = ul.children;
+    for (var i=0; i < li.length; i++) {
+        while(li[i] && li[i].children[0].checked) {
+            ul.removeChild(li[i]);
+        }
+    }
+}
